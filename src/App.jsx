ba9b2c1829e769e51e5ff60386d39d8408cd6e46
Sushil97 <sushil500530@gmail.com>
2023-09-17 18:34:48 +0600

    set the tailwind css for projects

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-6xl text-violet-500 font-bold text-center my-6'>Pricing Options</h1>
+
+     
+    </>
+  )
+}
+
+export default App
